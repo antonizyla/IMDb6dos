24609b4ebf2151c69787d93a65b6ad222aed277a
Antoni Zyla <zyla.antoni@gmail.com>
2023-02-11 19:43:38 +0000

    download and unzip data files from imdb

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,49 @@
 import { randomUUID } from "crypto"
 
-const { PrismaClient } = require('@prisma/client')
+import axios from 'axios'
+const fs = require('fs');
+const path = require('path');
+const zlib = require('zlib');
 
+const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
-const user = {
-    name: 'Alice',
-    email: `${randomUUID()}@gmail.com`
+
+async function fetchAndUnzip(filename: string, label = undefined) {
+
+    if (fs.existsSync(path.join(__dirname, `data/${filename}.tsv`))) {
+        console.log(`${label || filename} File Exists`)
+    } else {
+        console.log(`Fetching ${label || filename} file`)
+        const tsvData = await axios.get(`https://datasets.imdbws.com/${filename}.tsv.gz`, { responseType: 'arraybuffer', 'decompress': true }).then(res => {
+            return res.data;
+        }).catch((err: any) => {
+            console.log(err);
+            return;
+        });
+        console.log(`Fetched ${label || filename} file`)
+        fs.writeFileSync(path.join(__dirname, `data/${filename}.tsv.gz`), tsvData);
+
+        // unzip file
+        const contents = fs.createReadStream(path.join(__dirname, `data/${filename}.tsv.gz`));
+        const writestream = fs.createWriteStream(path.join(__dirname, `data/${filename}.tsv`));
+        const unzip = zlib.createGunzip();
+        let stream = contents.pipe(unzip).pipe(writestream);
+        console.log(`Unzipping ${label || filename} file`)
+
+        stream.on('finish', () => {
+            console.log(`Finished Unzipping ${label || filename} file`)
+            fs.unlinkSync(path.join(__dirname, `data/${filename}.tsv.gz`));
+        })
+    }
 }
 
+
 async function main() {
-    console.log(`Start seeding ...`)
-    const createdUser = await prisma.user.create({
-        data: user,
-    })
-    console.log(`Created user with id: ${createdUser.id}`)
+    const files = ['title.ratings', 'title.akas', 'title.basics', 'title.episode', 'title.principals', 'name.basics'];
+    files.forEach(async (file) => {
+        fetchAndUnzip(file);
+    });
 }
 
 main().then(async () => {
